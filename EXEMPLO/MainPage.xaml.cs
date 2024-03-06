e3cab1f1e8056dc84cc234de4bd67a955f9e3f2b
luana <luaninhadepaulamachado@gmail.com>
2024-03-06 18:20:07 -0300

    feat: add enviar mensagem

---
+++ EXEMPLO/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace EXEMPLO
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string name;
+
+        // método construtor
+        public MainPage() 
+        {
+            InitializeComponent();
+        }
+
+        private void OnMessageClicked(object sender, EventArgs e)
+        {
+            name = NameEntry.Text;
+            MessageLabel.Text = $"Hello, {name}!";
+        }
+    }
+}
